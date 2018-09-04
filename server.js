18b4f4fcd041b2c659ca9769e244f64539c070c4
Siddharth Kumar Singh <SidKumarSingh@users.noreply.github.com>
2018-09-04 17:12:09 +0530

    Mod 2 for POST handler

--- server.js
+++ server.js
@@ -3,9 +3,6 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-//const dialogflow = require('actions-on-google');
-
-//const dfapp = dialogflow();
 
 const app = express();
 
@@ -16,30 +13,31 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Example app listening on port 3000!'));
 
-app.all('/', (req, res, next) => {
+/*app.all('/', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
- });
+ });*/
 
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-	//console.log(req.body);
 
 	if(req.body.queryResult.action === 'get_data') {
-		let kpi = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.kpi ? req.body.queryResult.parameters.kpi : 'Unknown';
+		var kpi = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.kpi ? req.body.queryResult.parameters.kpi : 'Unknown';
+	} else {
+		var kpi = 'Unknown';
 	};
 
 	if(kpi==='Unknown') {
 		return res.json({
-			fulfillmentText: "KPI not found",
-			source: 'Qlik Sense'
+			"fulfillmentText": "KPI not found",
+			'source': 'Qlik Sense'
 		});
 	} else {
 		return res.json({
-			fulfillmentText: "We found KPI revenue for you",
-			source: 'Qlik Sense'
+			'fulfillmentText': "We found KPI revenue for you",
+			'source': 'Qlik Sense'
 		});
 	};
 
