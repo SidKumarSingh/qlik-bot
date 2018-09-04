649eef2edaa96c2bf29bcdd058e6d5e0ff2c524a
Siddharth Kumar Singh <SidKumarSingh@users.noreply.github.com>
2018-09-04 16:43:51 +0530

    Added DialogFlow POST handler

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-// content of index.js
+'use strict';
+
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+//const dialogflow = require('actions-on-google');
+
+//const dfapp = dialogflow();
+
 const app = express();
+
 
 app.get('/', (req, res) => res.send('Hello world from node.js server!'));
 
@@ -20,5 +26,24 @@ app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
 	//console.log(req.body);
-	res.send('Hello ' + req.body.fname + ' ' + req.body.lname + '! How are you?');
+
+	if(req.body.result.action === 'get_data') {
+		let kpi = req.body.result && req.body.result.parameters && req.body.result.parameters.kpi ? req.result.parameters.kpi : 'Unknown';
+	};
+
+	if(kpi==='Unknown') {
+		return res.json({
+			speech: 'KPI not found',
+			displayText: 'KPI not found',
+			source: 'Qlik Sense'
+		});
+	} else {
+		return res.json({
+			speech: 'We found kpi ' + kpi,
+			displayText: 'We found kpi ' + kpi,
+			source: 'Qlik Sense'
+		});
+	};
+
+	//res.send('Hello ' + req.body.fname + ' ' + req.body.lname + '! How are you?');
 });
