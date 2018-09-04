cb7677fbaaa3a425ae0b2bfdec90e90df372bcfe
Siddharth Kumar Singh <SidKumarSingh@users.noreply.github.com>
2018-09-04 12:47:51 +0530

    Update server.js with POST method for testing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // content of index.js
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello world from node.js server!'));
@@ -8,3 +9,16 @@ app.get('/', (req, res) => res.send('Hello world from node.js server!'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Example app listening on port 3000!'));
+
+app.all('/', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  next();
+ });
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+	//console.log(req.body);
+	res.send('Hello ' + req.body.fname + ' ' + req.body.lname + '! How are you?');
+});
