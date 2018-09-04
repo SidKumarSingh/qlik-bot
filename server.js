f91925b84582c1b0ebd1de3e80bc01c2aff255ba
Siddharth Kumar Singh <SidKumarSingh@users.noreply.github.com>
2018-09-04 17:02:43 +0530

    Modified POST handler for API v2

--- server.js
+++ server.js
@@ -27,20 +27,18 @@ app.use(bodyParser.json());
 app.post('/', (req, res) => {
 	//console.log(req.body);
 
-	if(req.body.result.action === 'get_data') {
-		let kpi = req.body.result && req.body.result.parameters && req.body.result.parameters.kpi ? req.result.parameters.kpi : 'Unknown';
+	if(req.body.queryResult.action === 'get_data') {
+		let kpi = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.kpi ? req.body.queryResult.parameters.kpi : 'Unknown';
 	};
 
 	if(kpi==='Unknown') {
 		return res.json({
-			speech: 'KPI not found',
-			displayText: 'KPI not found',
+			fulfillmentText: "KPI not found",
 			source: 'Qlik Sense'
 		});
 	} else {
 		return res.json({
-			speech: 'We found kpi ' + kpi,
-			displayText: 'We found kpi ' + kpi,
+			fulfillmentText: "We found KPI revenue for you",
 			source: 'Qlik Sense'
 		});
 	};
